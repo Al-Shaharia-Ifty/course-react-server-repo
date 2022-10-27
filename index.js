6f769403d1055fa1a2e38581afc395e1b43edcca
Al Shaharia <ifty682002@gmail.com>
2022-10-27 23:06:26 +0600

    course details

--- index.js
+++ index.js
@@ -11,11 +11,20 @@ const allCourses = require("./Data/courses.json");
 app.get("/", (req, res) => {
   res.send("Now server is running");
 });
-
+//
 app.get("/courses", (req, res) => {
   res.send(allCourses);
+});
+// single Course Details
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const courseDetails = allCourses.find((q) => q.id === id);
+  console.log(id);
+  res.send(courseDetails);
 });
 
 app.listen(Port, () => {
   console.log("server is running on", Port);
 });
+
+// module.exports = app;
